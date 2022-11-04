be7cf098e38c35ac8728bd3b14176a972d5624a7
canwdev <c2097@live.com>
2022-11-04 17:53:37 +0800

    background grid

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import {GlobalThemeOverrides, darkTheme} from 'naive-ui'
+import {darkTheme, GlobalThemeOverrides} from 'naive-ui'
 import AppContent from '@/AppContent.vue'
 import {useGlobalTheme} from '@/hooks/use-global-theme'
 
@@ -28,10 +28,10 @@ export default defineComponent({
 
 <template>
   <n-config-provider
-    :theme="isAppDarkMode ? darkTheme : null"
-    class="page-craft-root"
     :class="{_dark: isAppDarkMode}"
+    :theme="isAppDarkMode ? darkTheme : null"
     :theme-overrides="themeOverrides"
+    class="page-craft-root _line-grid"
   >
     <n-loading-bar-provider>
       <n-notification-provider>
@@ -45,21 +45,63 @@ export default defineComponent({
   </n-config-provider>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+#app {
+}
 .page-craft-root {
   position: relative;
   height: 100%;
   width: 100%;
-  background-color: aliceblue;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
   overflow: hidden;
-  //background-image: url('https://api.dujin.org/bing/1920.php');
-  background-image: url('@/assets/bg/day.png');
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    pointer-events: none;
+    z-index: 0;
+    background-image: url('@/assets/bg/grid-light.png');
+    background-size: 100px;
+    opacity: 0.4;
+  }
+  &::after {
+    content: 'PageCraft';
+    font-family: 'Minecraft';
+    color: #5480d3;
+    opacity: 0.6;
+    position: absolute;
+    right: 25px;
+    bottom: 10px;
+    pointer-events: none;
+    z-index: 1;
+  }
 
   &._dark {
-    background-image: url('@/assets/bg/twlight.png');
+    //background-color: #434343;
+    //background-image: linear-gradient(#434343, #282828);
+    background-color: #5480d3;
+    background-image: linear-gradient(#5480d3, #3256a7);
+    &::before {
+      background-image: url('@/assets/bg/grid.png');
+    }
+    &::after {
+      color: white;
+      text-shadow: 0 0 2px #000;
+    }
+  }
+
+  &._mc-bg {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-image: url('@/assets/bg/day.png');
+
+    &._dark {
+      background-image: url('@/assets/bg/twlight.png');
+    }
   }
 }
 </style>
