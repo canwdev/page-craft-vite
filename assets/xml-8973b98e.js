a1047830aedfe38b0dd0ba821714026f0dd577ba
canwdev <c2097@live.com>
2023-09-04 08:44:51 +0000

    Deploying to gh-pages from @ canwdev/page-craft-vite@a25286bae65aa34f4d8b0eef66904e846454531a 🚀

--- assets/xml-8973b98e.js
+++ assets/xml-8973b98e.js
@@ -1,4 +1,4 @@
-import{b7 as c}from"./index-774b188f.js";/*!-----------------------------------------------------------------------------
+import{b7 as c}from"./index-26228446.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
